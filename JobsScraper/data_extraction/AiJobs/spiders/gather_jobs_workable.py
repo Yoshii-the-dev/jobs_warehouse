d6facf43c3afa99a93e748f997b29e4b77b1c794
deviannt <egorkitov30@gmail.com>
2024-02-01 15:21:59 +0300

    Update gather_jobs_workable.py

--- JobsScraper/data_extraction/AiJobs/spiders/gather_jobs_workable.py
+++ JobsScraper/data_extraction/AiJobs/spiders/gather_jobs_workable.py
@@ -56,7 +56,7 @@ class WorkableJobSpider(scrapy.Spider):
         driver.page_source
         wait = WebDriverWait(driver, 5)
     
-        clicks = 30
+        clicks = 5
         while clicks>=0:
             try:
                 clicks -= 1
